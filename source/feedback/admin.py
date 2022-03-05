64c561b2425cfaf8da199bfa14a9edf21d848a69
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-03-05 13:57:31 +0600

    Реализовал первый этап!

--- source/feedback/admin.py
+++ source/feedback/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from feedback.models import Product, Review
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name_goods', 'description']
+    list_filter = ['category']
+    search_fields = ['category']
+    fields = ['name_goods', 'category', 'description', 'picture']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Review)
--- source/feedback/models.py
+++ source/feedback/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORY_CHOICES = [("other", "Other"), ("laptops", "Laptops"), ("monitors", "Monitors"),
+                    ("office_equipment", "Office equipment"), ("video_surveillance", "Video surveillance")]
+
+
+class Product(models.Model):
+    name_goods = models.CharField(max_length=100, verbose_name='Наименование товара')
+    category = models.CharField(max_length=20, default='other',
+                                choices=CATEGORY_CHOICES, verbose_name='Категории')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Текст записи')
+    picture = models.ImageField(verbose_name="Картинка", upload_to="pictures/", default='/pictures/product.jpg', null=True, blank=True)
+
+
+class Review(models.Model):
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='', verbose_name='Автор')
+    product = models.ForeignKey('feedback.Product', on_delete=models.CASCADE, related_name='review', verbose_name='Продукт')
+    text = models.TextField(max_length=2000, verbose_name='Текст отзыва')
+    mark = models.IntegerField(verbose_name="Оценка")
+    moderate = models.BooleanField(verbose_name="Модерирование")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    edited_at = models.DateTimeField(auto_now=True, verbose_name="Время редактирования")
